bf6e7adc0214c4c008772dbaca9082dc3bdf2632
SakuraNene <skrnn0505@gmail.com>
2025-07-06 21:18:25 +0900

    update

--- include/NeneEngine/NeneServer.hpp
+++ include/NeneEngine/NeneServer.hpp
@@ -1,6 +1,27 @@
 // 依存性グループB: NeneServer
 #pragma once
+#include <map>
+#include <memory>
+#include <string>
+#include <SDL3/SDL.h>
 #include <NeneObject.hpp>
 
 class NeneServer {
+public:
+    NeneServer(); // 引数を追加する. 引数に基づいて初期化する.
+    ~NeneServer();
+    std::shared_ptr<SDL_Window> window;
+    std::shared_ptr<SDL_Renderer> renderer;
+    std::shared_ptr<NeneViewWorld> view_world;
+    std::shared_ptr<NeneCamera> camera;
+};
+
+class NeneViewWorld {
+public:
+    std::map<int, NeneViewObject> objects;
+};
+
+class NeneCamera {
+public:
+    void shoot();
 };
